6f2cc8f29e9d9a21a6fb7f7ec86f982db18ea6ea
Doston Hamrakulov <doston.hamrakulov@gmail.com>
2018-02-01 23:03:03 +0200

    Starter activity

--- app/src/main/java/rtu/group/adaptive/renthouse/MainActivity.java
+++ app/src/main/java/rtu/group/adaptive/renthouse/MainActivity.java
@@ -1,7 +1,9 @@
 package rtu.group.adaptive.renthouse;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void Staring(View view){
+        startActivity(new Intent(MainActivity.this, Starter.class));
     }
 }
