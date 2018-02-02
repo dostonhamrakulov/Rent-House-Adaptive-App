7eaed5866ee992fb23b34547a40690d7bc6c626a
Doston Hamrakulov <doston.hamrakulov@gmail.com>
2018-02-02 03:02:05 +0200

    Design is done

--- app/src/main/java/rtu/group/adaptive/renthouse/AdaptiveDisplay.java
+++ app/src/main/java/rtu/group/adaptive/renthouse/AdaptiveDisplay.java
@@ -1,8 +1,14 @@
 package rtu.group.adaptive.renthouse;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class AdaptiveDisplay extends AppCompatActivity {
 
@@ -24,5 +30,67 @@ public class AdaptiveDisplay extends AppCompatActivity {
         backgroundTask.execute("get_adaptive");
 
         getSupportActionBar().setDisplayShowHomeEnabled(true);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.activity_main_action, menu);
+
+        //getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            Toast.makeText(AdaptiveDisplay.this, "Sorry, there is no any option for setting!!!", Toast.LENGTH_LONG).show();
+            return true;
+        }
+        switch(id){
+            case R.id.id_about_us:
+                Toast.makeText(AdaptiveDisplay.this, "I am Junior Android Developer!!!", Toast.LENGTH_LONG).show();
+                return true;
+            case R.id.id_contact_us:
+                Toast.makeText(AdaptiveDisplay.this, "If you want to cantact me, please write email to doston2509@gmail.com!!!",
+                        Toast.LENGTH_LONG).show();
+                return true;
+            case R.id.id_exit_app:
+                Exit_alert();
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+    public void Exit_alert(){
+        AlertDialog.Builder alert_builder = new AlertDialog.Builder(this);
+        alert_builder.setMessage("Do you want to close this app?")
+                .setCancelable(false)
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Toast.makeText(AdaptiveDisplay.this, "Good bye!!!", Toast.LENGTH_SHORT).show();
+                        moveTaskToBack(true);
+                        android.os.Process.killProcess(android.os.Process.myPid());
+                        System.exit(1);
+                    }
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.cancel();
+                    }
+                });
+
+        AlertDialog alert = alert_builder.create();
+        alert.setTitle("Alert!!!");
+        alert.show();
     }
 }
--- app/src/main/java/rtu/group/adaptive/renthouse/MainActivity.java
+++ app/src/main/java/rtu/group/adaptive/renthouse/MainActivity.java
@@ -1,9 +1,15 @@
 package rtu.group.adaptive.renthouse;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,5 +20,68 @@ public class MainActivity extends AppCompatActivity {
     }
     public void Staring(View view){
         startActivity(new Intent(MainActivity.this, Starter.class));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.activity_main_action, menu);
+
+        //getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            Toast.makeText(MainActivity.this, "Sorry, there is no any option for setting!!!", Toast.LENGTH_LONG).show();
+            return true;
+        }
+        switch(id){
+            case R.id.id_about_us:
+                Toast.makeText(MainActivity.this, "I am Junior Android Developer!!!", Toast.LENGTH_LONG).show();
+                return true;
+            case R.id.id_contact_us:
+                Toast.makeText(MainActivity.this, "If you want to cantact me, please write email to doston2509@gmail.com!!!",
+                        Toast.LENGTH_LONG).show();
+                return true;
+            case R.id.id_exit_app:
+                Exit_alert();
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+    public void Exit_alert(){
+        AlertDialog.Builder alert_builder = new AlertDialog.Builder(this);
+        alert_builder.setMessage("Do you want to close this app?")
+                .setCancelable(false)
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Toast.makeText(MainActivity.this, "Good bye!!!", Toast.LENGTH_SHORT).show();
+                        moveTaskToBack(true);
+                        android.os.Process.killProcess(android.os.Process.myPid());
+                        System.exit(1);
+                    }
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.cancel();
+                    }
+                });
+
+        AlertDialog alert = alert_builder.create();
+        alert.setTitle("Alert!!!");
+        alert.show();
     }
 }
