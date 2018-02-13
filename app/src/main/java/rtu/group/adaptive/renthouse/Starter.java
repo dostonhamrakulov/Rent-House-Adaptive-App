14e5a802ee46723cc1a323ca9697efd3a9620834
Doston Hamrakulov <doston.hamrakulov@gmail.com>
2018-02-13 12:12:56 +0200

    Publishing

--- app/src/main/java/rtu/group/adaptive/renthouse/Starter.java
+++ app/src/main/java/rtu/group/adaptive/renthouse/Starter.java
@@ -20,9 +20,9 @@ public class Starter extends AppCompatActivity {
         setContentView(R.layout.activity_starter);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
     }
-//    public void moveInserting(View view){
-//        startActivity(new Intent(Starter.this, Inserting.class));
-//    }
+    public void moveInserting(View view){
+        startActivity(new Intent(Starter.this, Inserting.class));
+    }
     public void moveDisplaying(View view){
         startActivity(new Intent(Starter.this, Displaying.class));
     }
